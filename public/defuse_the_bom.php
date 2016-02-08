1f37ca52ebf085690db47fb42ee28fa60994ecc2
Anna Morton <annajmorton@gmail.com>
2016-02-08 15:58:04 -0600

    some more changes

--- public/defuse_the_bom.php
+++ public/defuse_the_bom.php
@@ -23,9 +23,7 @@
     <h2 id="message">This BOM will self destruct in <span id="timer">5</span> seconds...</h2>
 
     
-    <img src="./img/bomb.jpg" id ="bom" alt="bomb">
-
-    <img src="./img/thumbs-up.png" id="thumbs-up" alt="thumbs-up">
+    <img src="./img/bomb.jpg" id ="page-img" alt="bomb">
 
     <br>
 
@@ -39,27 +37,24 @@
     <script>
         var detonationTimer = 5;
         var interval = 1000;
-
-        function setImageVisible(id, visible) {
-            var img = document.getElementById(id);
-            img.style.visibility = (visible ? 'visible' : 'hidden');
-        }
+        var pageImg = document.getElementById("page-img");
 
 
         // TODO: This function needs to be called once every second
         function updateTimer()
         {
             
-            setImageVisible("thumbs-up",false);
-
             if (detonationTimer == 0) {
                 alert('EXTERMINATE!');
                 document.body.innerHTML = '';
+
+
             } else if (detonationTimer > 0) {
                 document.getElementById('timer').innerHTML = detonationTimer;
             }
 
             detonationTimer--;
+
         }
 
         var intervalId = setInterval(updateTimer, interval);
@@ -69,9 +64,10 @@
         function defuseTheBOM()
         {
             clearInterval(intervalId);
+            pageImg.setAttribute("src", "./img/thumbs-up.png")
+            back
+
             alert("Good work Agent!!!!");
-            setImageVisible("bom", false);
-            setImageVisible("thumbs-up",true);
 
         }
 
