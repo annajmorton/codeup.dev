2a1e39e6c5474d204b598c6c1426472ff3ec162e
Anna Morton <annajmorton@gmail.com>
2016-02-26 10:49:42 -0600

    more changes

---
+++ public/constants.php
@@ -0,0 +1,7 @@
+<?php
+define("MY_NAME", "Anna Morton");
+echo MY_NAME . PHP_EOL;
+$nameTest = "Stan";
+// $nameTest
+echo "helloooo PHP i'm glad to know you. My name is " . MY_NAME . PHP_EOL . PHP_EOL;
+echo "helloooo PHP i'm glad to know you. Your name is $nameTest" . PHP_EOL ;
